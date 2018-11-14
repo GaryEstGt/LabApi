cd923368508075db34a5133989c39c6345cfcd0b
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-11-14 15:03:10 -0600

    Crear servidor
    creacion de servidor

---
+++ Index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// initialize express app
+const app = express();
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Servidor Iniciado en el puerto: n' + port);
+});
