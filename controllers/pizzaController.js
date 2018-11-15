5c374029071dc16d51655cd824c3f93f3259d654
Luis Farfán <luise.prado99@hotmail.com>
2018-11-14 22:00:50 -0600

    Corregir errores controller

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -5,6 +5,7 @@ const tok= require('../models/tokenModel');
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
+
 exports.pizza_create = function (req, res) {
     let piza = new pizza(
         {
@@ -23,30 +24,35 @@ exports.pizza_create = function (req, res) {
         res.send('Pizza Created successfully')
     })
 };
+
 exports.pizza_details = function (req, res) {
     pizza.find({}, function (err, pizza) {
         if (err) return next(err);
         res.send(pizza);
     })
 };
+
 exports.pizza_detailsone = function (req, res) {
     pizza.findOne(req.body.nombre, function (err, pizza) {
         if (err) return next(err);
         res.send(pizza);
     })
 };
+
 exports.pizza_update = function (req, res) {
     pizza.findOneAndUpdate(req.body.nombre, {$set: req.body}, function (err, pizza) {
         if (err) return next(err);
         res.send('Pizza udpated.');
     });
 };
+
 exports.pizza_delete = function (req, res) {
     pizza.findOneAndRemove(req.body.nombre, function (err) {
         if (err) return next(err);
         res.send('Deleted successfully!');
     })
 };
+
  exports.pizza_verificar = function (req, res) {
     tok.findOne(req.body.nombre, function (err, pizza) {
         if (err) return next(err);
